bb837f2d9f4add52c1a843bec901f0e6b5550f4e
David Walker <david@davidwalker.org>
2017-02-14 11:45:06 +0000

    Add history for completed items

--- src/cljs/pushpopchestnutreless/core.cljs
+++ src/cljs/pushpopchestnutreless/core.cljs
@@ -7,10 +7,12 @@
 
 (defonce app-state
   (atom
-    {:stack (list "first" "second")
+    {:stack ()
      :new-item-text ""
      :snooping? false
-     :filter-text ""}))
+     :filter-text ""
+     :completed ()
+     :showing-history? false}))
 
 (defmulti step
   (fn [_ {:keys [id]}]
@@ -27,13 +29,18 @@
     (assoc :new-item-text "")))
 
 (defmethod step :pop [state _]
-  (update state :stack pop))
+  (-> state
+   (update :completed conj (peek (:stack state)))
+   (update :stack pop)))
 
 (defmethod step :toggle-snoop [state _]
   (update state :snooping? not))
 
 (defmethod step :filter [state {:keys [text]}]
   (assoc state :filter-text text))
+
+(defmethod step :toggle-history [state _]
+  (update state :showing-history? not))
 
 
 ;; VIEW HELPERS
@@ -56,13 +63,12 @@
 
 ;; VIEW
 
-(defn peek-view [{:keys [stack filter-text snooping?]}]
+(defn peek-view [{:keys [stack completed filter-text snooping? showing-history?]}]
  (when (not-empty stack)
   [:div.peek-container
-   [:p
-    [:a
-     {:on-click (on-click :toggle-snoop)}
-     (if snooping? "peeking..." "peek...")]]
+   [:h3
+    {:on-click (on-click :toggle-snoop)}
+    (if snooping? "Peeking..." "Peek...")]
    (when snooping?
      [:div
       [:input
@@ -71,7 +77,17 @@
       (into [:ol]
        (->> stack
             (filter #(string/includes? % filter-text))
-            (map #(vec [:li %]))))])]))
+            (map #(vec [:li %]))))
+      [:h4
+       {:on-click (on-click :toggle-history)}
+       (if showing-history? "Hide History" "Show History")]
+      [:div
+       (when showing-history?
+        (into [:ol.history]
+         (->> completed
+              (filter #(string/includes? % filter-text))
+              (map #(vec [:li %])))))]])]))
+
 
 (defn push-pop-view [{:keys [stack filter-text] :as state}]
   [:div

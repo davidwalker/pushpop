e3fd1c1cea8606b4d40661e1ce4d0ff1ebacdb01
David Walker <david@davidwalker.org>
2017-02-14 00:11:06 +0000

    Add counter component

--- src/cljs/pushpopchestnutreless/core.cljs
+++ src/cljs/pushpopchestnutreless/core.cljs
@@ -9,6 +9,8 @@
   (fn [{:keys [id]}]
    id))
 
+
+;; UPDATE
 (defmethod step :change-text [m]
   (swap! app-state assoc :text (:text m)))
 
@@ -18,7 +20,15 @@
 (defmethod step :dec [m]
   (swap! app-state update :count dec))
 
+(defmethod step :inc2 [m]
+  (swap! app-state update :count2 inc))
 
+(defmethod step :dec2 [m]
+  (swap! app-state update :count2 dec))
+
+
+
+; VIEW HELPERS
 
 (defn on-change [msg-id]
   #(step {:id msg-id
@@ -28,19 +38,32 @@
   #(step {:id msg-id}))
 
 
-(defn greeting []
-  (.log js/console "render greeting")
+;: VIEW
+
+(defn counter [count inc-msg dec-msg]
+  (.log js/console (str "render counter " inc-msg))
   [:div
-   [:h1 (:text @app-state)]
-   [:input 
-    {:on-change (on-change :change-text)
-     :value (:text @app-state)}]
-   [:h2 (str "count " (:count @app-state))]
+   [:h2 (str "count " count)]
    [:button 
-    {:on-click (on-click :inc)}
+    {:on-click (on-click inc-msg)}
     "+"]
    [:button 
-    {:on-click (on-click :dec)}
+    {:on-click (on-click dec-msg)}
     "-"]])
 
-(reagent/render [greeting] (js/document.getElementById "app"))
+(defn greeting [state]
+  (.log js/console "render greeting")
+  [:div
+   [:h1 (:text state)]
+   [:input 
+    {:on-change (on-change :change-text)
+     :value (:text state)}]
+   [counter (:count state) :inc :dec]
+   [counter (:count2 state) :inc2 :dec2]])
+
+
+(defn app []
+  (.log js/console "render app")
+  [greeting @app-state])
+
+(reagent/render [app] (js/document.getElementById "app"))
